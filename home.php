d417b34dd0d170d14d5bd13cc5f086bff8067a7c
FajarAthariq <100130787+FajarAthariq@users.noreply.github.com>
2022-05-10 22:34:27 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,113 @@
+<?php include 'db_connect.php' ?>
+<link rel="icon" href="img/345678.jpg"/>
+<style>
+   span.float-right.summary_icon {
+    font-size: 3rem;
+    position: absolute;
+    right: 1rem;
+    top: 0;
+}
+.imgs{
+		margin: .5em;
+		max-width: calc(100%);
+		max-height: calc(100%);
+	}
+	.imgs img{
+		max-width: calc(100%);
+		max-height: calc(100%);
+		cursor: pointer;
+	}
+	#imagesCarousel,#imagesCarousel .carousel-inner,#imagesCarousel .carousel-item{
+		height: 60vh !important;background: black;
+	}
+	#imagesCarousel .carousel-item.active{
+		display: flex !important;
+	}
+	#imagesCarousel .carousel-item-next{
+		display: flex !important;
+	}
+	#imagesCarousel .carousel-item img{
+		margin: auto;
+	}
+	#imagesCarousel img{
+		width: auto!important;
+		height: auto!important;
+		max-height: calc(100%)!important;
+		max-width: calc(100%)!important;
+	}
+</style>
+
+<div class="containe-fluid">
+	<div class="row mt-3 ml-3 mr-3">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <?php echo "Selamat Datang ". $_SESSION['login_name']."!"  ?>
+                    <hr><?php echo "Pada menu ini dapat mengakses beberapa tools, diantaranya:<br>1. Tiketing<br>   Berfungsi untuk 
+			memesan tiket dan melakukan pembayaran tiket<br>2. Stations<br>   Tools ini akan memperlihatkan beberapa stasiun yang tersedia
+			<br>3. Price<br>Menunjukkan harga tiap satuan tiket sesuai dengan jarak stasiun dan kategori penumpang, seperti dewasa atau pelajar
+			<br>4. Daily Report<br>Menunjukkan laporan dari transaksi penjualan tiket<br>5. Users<br>Memperlihatkan user yang menggunakan sistem
+			ini sesuai dengan level"  ?>
+                </div>
+            </div>      			
+        </div>
+    </div>
+</div>
+<script>
+	$('#manage-records').submit(function(e){
+        e.preventDefault()
+        start_load()
+        $.ajax({
+            url:'ajax.php?action=save_track',
+            data: new FormData($(this)[0]),
+            cache: false,
+            contentType: false,
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+            success:function(resp){
+                resp=JSON.parse(resp)
+                if(resp.status==1){
+                    alert_toast("Data successfully saved",'success')
+                    setTimeout(function(){
+                        location.reload()
+                    },800)
+
+                }
+                
+            }
+        })
+    })
+    $('#tracking_id').on('keypress',function(e){
+        if(e.which == 13){
+            get_person()
+        }
+    })
+    $('#check').on('click',function(e){
+            get_person()
+    })
+    function get_person(){
+            start_load()
+        $.ajax({
+                url:'ajax.php?action=get_pdetails',
+                method:"POST",
+                data:{tracking_id : $('#tracking_id').val()},
+                success:function(resp){
+                    if(resp){
+                        resp = JSON.parse(resp)
+                        if(resp.status == 1){
+                            $('#name').html(resp.name)
+                            $('#address').html(resp.address)
+                            $('[name="person_id"]').val(resp.id)
+                            $('#details').show()
+                            end_load()
+
+                        }else if(resp.status == 2){
+                            alert_toast("Unknow tracking id.",'danger');
+                            end_load();
+                        }
+                    }
+                }
+            })
+    }
+</script>
